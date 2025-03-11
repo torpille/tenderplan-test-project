3c23823852eabb52baa608fe9f6a0f2ea0ddaaa6
lisa <torpille113@gmail.com>
2025-03-11 08:50:41 +0300

    добавлен докер контейнер

--- celery_app.py
+++ celery_app.py
@@ -1,5 +1,5 @@
 from celery import Celery
 
-app = Celery('my_app', broker='redis://localhost:6379/0',
-             result_backend='redis://localhost:6379/0')
+app = Celery('my_app', broker='redis://redis:6379/0',
+             result_backend='redis://redis:6379/0')
 app.autodiscover_tasks(['tasks'])
